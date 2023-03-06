0bd7cddb454e91588aa2b260c6a712135142bacf
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-03-06 13:14:38 -0300

    ADD: Icons with react-icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import { BsCartCheck } from 'react-icons/bs'
 
 const Header = () => {
   return (
-    <header className='Header'>
-      <h1 className='Header-title'> Conf Merch</h1>
-      <div className='Header-checkout'>
-        <p>Checkout</p>
+    <header className="Header">
+      <h1 className="Header-title">
+        <Link to="/">Conf Merch</Link>
+      </h1>
+      <div className="Header-checkout">
+        <Link to="/checkout"><BsCartCheck size={20} /></Link>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export { Header }
+export { Header };
--- src/containers/Checkout/Checkout.jsx
+++ src/containers/Checkout/Checkout.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import './Checkout.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Checkout.css";
+import { BsTrash } from 'react-icons/bs'
 
 const Checkout = () => {
   return (
-    <div className='Checkout'>
+    <div className="Checkout">
       <div className="Checkout-content">
         <h3>Lista de pedidos:</h3>
         <div className="Checkout-item">
@@ -11,15 +13,19 @@ const Checkout = () => {
             <h4>ITEM name</h4>
             <span>$10</span>
           </div>
-          <button className='fas' type="button">Eliminar</button>
+          <button className="fas" type="button">
+            <BsTrash size={15} />
+          </button>
         </div>
       </div>
       <div className="Checkout-sidebar">
         <h3>Precio total: $10</h3>
-        <button type="button">Continuar pedido</button>
+        <Link to="/checkout/information">
+          <button type="button">Continuar pedido</button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export { Checkout }
+export { Checkout };
