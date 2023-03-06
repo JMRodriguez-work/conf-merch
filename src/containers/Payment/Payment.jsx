d46ab69eb0e85dd7cb05d109b9b5a78d4522a9cf
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-03-06 12:49:09 -0300

    ADD: Payment and Success content and styles

--- src/containers/Payment/Payment.jsx
+++ src/containers/Payment/Payment.jsx
@@ -3,7 +3,15 @@ import './Payment.css';
 
 const Payment = () => {
   return (
-    <div className='Payment'>Payment</div>
+    <div className='Payment'>
+      <div className="Payment-content">
+        <h3>Resumen del pedido:</h3>
+        <div className="Payment-button">
+          Boton de pago con Paypal
+        </div>
+      </div>
+      <div></div>
+    </div>
   )
 }
 
--- src/containers/Success/Success.jsx
+++ src/containers/Success/Success.jsx
@@ -3,7 +3,15 @@ import './Success.css';
 
 const Success = () => {
   return (
-    <div className='Success'>Success</div>
+    <div className='Success'>
+      <div className="Success-content">
+        <h2>Martin, Gracias por tu compra</h2>
+        <span>Tu pedido llegará en 3 dias a tu dirección:</span>
+        <div className="Success-map">
+          Google Maps
+        </div>
+      </div>
+    </div>
   )
 }
 
